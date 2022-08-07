efa94ab9c8f2117e0578373aa6090dbeaa824d10
yanbo92 <13750429758@139.com>
2022-08-08 00:40:32 +0800

    update some optimize about duplicate removing

--- rent_helper.py
+++ rent_helper.py
@@ -8,18 +8,9 @@ class RentHelper(Session):
         super().__init__()
         self.config = read_json("config.json")
         self.url = self.config["url"]
+        self.filters = self.config["filter"]
         self.params = self.config["params"]
         self.result = self.run()
-
-    def run(self) -> dict:
-        result = {}
-        for param in self.params:
-            datalist = self.get_datalist(param=param)
-            for data in datalist:
-                # time style 2022-08-07 22:59:41
-                if time.mktime(time.strptime(data["pub_time"], '%Y-%m-%d %H:%M:%S')) - time.time() < 86400:
-                    result[data["url"]] = data["title"]
-        return result
 
     def get_datalist(self, param: dict) -> dict:
         resp = super().get(url=self.url, params=param).json()
@@ -27,7 +18,17 @@ class RentHelper(Session):
             return resp["data"]["dataList"]
         assert False, f"get data from {self.url} failed! code={resp['code']}, msg={resp['msg']}"
 
+    def run(self) -> dict:
+        result = {}
+        for param in self.params:
+            datalist = self.get_datalist(param=param)
+            for data in datalist:
+                # time style 2022-08-07 22:59:41
+                # within 8 hours
+                if abs(time.mktime(time.strptime(data["pub_time"], '%Y-%m-%d %H:%M:%S')) - time.time()) < 29000:
+                    for f in self.filters:
+                        if f in data["title"]:
+                            result[data["url"]] = data["title"]
+        return result
 
-if __name__ == '__main__':
-    print(RentHelper().result)
 
--- telegram_sender.py
+++ telegram_sender.py
@@ -12,10 +12,16 @@ class TelegramSender:
     def generate_markdown(self, house) -> str:
         return f"[{self.houses[house]}]({house})"
 
-    def send_markdown(self, text, chat_id):
+    def generate_normal_text(self, house) -> str:
+        return f"url:{self.houses[house]}, title:{house}"
+
+    def send_message(self, text, chat_id):
         self.bot.send_message(text=text, chat_id=chat_id, parse_mode="Markdown")
 
     def run(self):
         for house in self.houses.keys():
-            self.send_markdown(text=self.generate_markdown(house), chat_id=self.chat)
+            try:
+                self.send_message(text=self.generate_markdown(house), chat_id=self.chat)
+            except Exception:
+                self.send_message(text=self.generate_normal_text(house), chat_id=self.chat)
             time.sleep(3)
