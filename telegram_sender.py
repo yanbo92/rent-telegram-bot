a784614d8b7fd3ed26e3230072c2147d2268b774
yanbo92 <13750429758@139.com>
2022-08-08 00:59:32 +0800

    update telegram_sender for debug

--- telegram_sender.py
+++ telegram_sender.py
@@ -15,13 +15,16 @@ class TelegramSender:
     def generate_normal_text(self, house) -> str:
         return f"url:{self.houses[house]}, title:{house}"
 
-    def send_message(self, text, chat_id):
+    def send_markdown(self, text, chat_id):
         self.bot.send_message(text=text, chat_id=chat_id, parse_mode="Markdown")
+
+    def send_text(self, text, chat_id):
+        self.bot.send_message(text=text, chat_id=chat_id)
 
     def run(self):
         for house in self.houses.keys():
             try:
-                self.send_message(text=self.generate_markdown(house), chat_id=self.chat)
+                self.send_markdown(text=self.generate_markdown(house), chat_id=self.chat)
             except Exception:
-                self.send_message(text=self.generate_normal_text(house), chat_id=self.chat)
+                self.send_text(text=self.generate_normal_text(house), chat_id=self.chat)
             time.sleep(3)
